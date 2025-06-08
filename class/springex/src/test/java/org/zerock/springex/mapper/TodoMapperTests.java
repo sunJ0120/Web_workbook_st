03bd4f59a21b34851b03f5819c883fee86a678c3
sunJ0120 <sspure1214@gmail.com>
2025-06-08 15:54:52 +0900

    Feat: 부트스트랩 적용 및 MyBatis 연동 및 test 완료

--- class/springex/src/test/java/org/zerock/springex/mapper/TodoMapperTests.java
+++ class/springex/src/test/java/org/zerock/springex/mapper/TodoMapperTests.java
@@ -1,21 +1,22 @@
 package org.zerock.springex.mapper;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@Slf4j
+@Log4j2
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/root-context.xml")
-class TimeMapperTest {
+public class TodoMapperTests {
     @Autowired(required = false)
-    private TimeMapper2 timeMapper2;
+    private TodoMapper todoMapper;
 
     @Test
-    public void testGetTime(){
-        log.info("timeMapper2.getNow() = {}", timeMapper2.getNow());
+    public void testGetTime() {
+        //<select id="getTime" resultType="string"> 이므로 todoMapper.getTime()로 test한다.
+        log.info("todoMapper.getTime() = {}", todoMapper.getTime());
     }
 }
